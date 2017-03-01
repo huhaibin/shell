663a1fde46334ef6fad16d379892761c29c69782
haibin <abing_hu@icloud.com>
2017-03-01 17:32:51 +0800

    新增可配置化常用软件安装脚本

---
+++ linux_common_software.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 配置要安装的常用软件
+softwareName=(# =两边不能有空格
+git
+bash-completion # bash命令参数自动补全，CentOS7新版不会自动安装此工具包
+npm # nodejs包管理器
+nginx
+composer # php包管理器
+redis
+)
+
+# 拼接常用软件命令
+nameList=""
+for name in ${softwareName[@]}
+do
+    nameList=${nameList}" "${name}
+done
+
+# 批量安装常用软件
+sudo yum install -y ${nameList}
