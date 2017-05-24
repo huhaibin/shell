d3f0e3836830ae2822d500b97606a026ef1fac8d
haibin <abing_hu@icloud.com>
2017-05-24 15:22:42 +0800

    调整测试docker脚本

--- install_docker.sh
+++ install_docker.sh
@@ -6,9 +6,6 @@ sudo yum instal docker
 # 启动docker服务
 sudo systemctl start docker
 
-# 将当前用户加入dockerroot组（方便直接使用docker命令，否则需要sudo使用root权限），
-sudo usermod -aG dockerroot $USER
-
 # 测试docker服务
 echo "测试docker hello world!"
-docker run hello-world
+sudo docker run hello-world
